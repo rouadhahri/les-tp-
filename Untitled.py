fc3f78b2592dc7bf7f91b7ef340fe9a3c4e082be
rouadhahri <92182374+rouadhahri@users.noreply.github.com>
2021-11-10 18:39:54 +0100

    Add files via upload

---
+++ Untitled.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def prod(x,y):
+    ''' Cette fonction effectue le produit de deux nombre
+    '''
+    return x*y # cett el
+
+
+# In[2]:
+
+
+prod(5,6)
+
+
+# In[ ]:
+
+
+
+
