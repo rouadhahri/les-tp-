5223a67a9b3d9fc6c7bde27420d2f479bdb07d11
rouadhahri <92182374+rouadhahri@users.noreply.github.com>
2021-11-11 20:48:47 +0100

    Update Untitled.py

--- Untitled.py
+++ Untitled.py
@@ -1,27 +1,12 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
-def prod(x,y):
-    ''' Cette fonction effectue le produit de deux nombre
-    '''
-    return x*y # cett el
-
-
-# In[2]:
-
-
-prod(5,6)
-
-
-# In[ ]:
-
-somme=lambda x,y,z: x+y+z
-print(somme(12,12,3))
-
-print(somme(0,0,0))
+import matplotlib.pyplot as plt
+from numpy import linspace
+f=lambda x: x**3+x**2-3*x-3
+T=linspace(-2,2,41)
+plt.title("solution dans [-2:2] ");
+plt.xlabel("x")
+plt.ylabel ("f(x)")
+plt.grid(True)
+plt.plot(T,f(T))
 
 
 
